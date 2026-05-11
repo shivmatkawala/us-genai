44692d2cdce903c711aaff3cfc0b4b29003b0c6f
shivmatkawala <shivkumarmatkawala@gmail.com>
2026-05-11 22:30:13 +0530

    python variable

---
+++ 1_intro.py
@@ -0,0 +1,23 @@
+# What type of Language is Python ?
+
+    # 1) Python is General Purpose Langauge ?
+        # Development
+        # Automation
+        # Scripting
+        # AI/ ML/ DL
+        # Datascience
+        # Cyber Security
+
+    # 2) Python is High Level Language ?
+        # If a language is human understandable then it is High level language.
+
+    # 3) Python is English like Language
+
+    # 4) Python is Dynamically Typed Language
+
+    # 5) Python is case sensitive Language
+            # x = 5 , X = 10 
+
+    # 6) Python is Hybrid Language (Compiler + Interpreter)
+
+    # 7) Python supports Procedural, Functional, Object Oriented, Modular Programming Paradigm
---
+++ 2_variables.py
@@ -0,0 +1,43 @@
+# Variables:-
+
+    # Variables are nothing but references for values
+    # We can access information/ data/ value using variables
+    # variables are also containers to store data
+
+my_name = 'Rushil'
+print(my_name)
+
+
+# Rules to create variable :
+
+    # To create a varible you can use english Alphabets (Capital, Lower)
+    # To create a variable you can use number but number must not present at start of the variable.
+        # 5person  ==> Wrong
+        # person5 ==> Correct
+        # per5son ==> Correct
+
+    # To create a variable do not use special charecter except _
+
+    # To create a variable do not use keywords:-
+            # Keywords are those words which python software uses internally
+
+# import keyword
+# print(keyword.kwlist)
+
+
+# 'False', 'None', 'True', 'and', 'as', 'assert', 'async', 
+# 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 
+# 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 
+# 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 
+# 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield'
+
+
+# Create meaningful variables 
+
+# x = 'Alan'
+# sjkfskf = 55
+
+name = 'Alan'
+weight = 55
+
+
