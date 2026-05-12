afc5feaefccd2d9616771461cc227656db3d8732
shivmatkawala <shivkumarmatkawala@gmail.com>
2026-05-12 22:38:28 +0530

    string started

---
+++ 3_datatypes_and_numeric.py
@@ -0,0 +1,38 @@
+# Data Types :-
+
+    # Numeric:-
+        # 1) Integer :- 
+            # Any Number without decimal point is integer
+            # Integeres can be +ve and -ve
+            # including 0
+
+            # Ex:-  20, 1, 2, 40, 0, -50, -1, -78
+
+        # 2) Float:-  
+            # Any Number with decimal point is float
+            # Float can be +ve or -ve
+
+            # Ex: 1.14, 0.000, -23.56, -0.123
+
+        # 3) Complex:- A combination of Real number and Imaginary number.
+            # Real Number :- All Ints and Floats
+            # Imaginary Number:- A number with j prefix
+
+            # Ex: 5+7j,  -12-45j, -0.56+4.5j
+
+
+# height = 167
+# print(height)
+# print(type(height))  # int
+
+# weight = 78.45
+# print(weight)
+# print(type(weight))  # float
+
+# x = 4-7j
+# print(x)
+# print(type(x))  # complex
+
+
+
+
---
+++ 4_string_datatype.py
@@ -0,0 +1,119 @@
+# String:-
+
+    # Any charecter or sequence of charecters enclosed 
+    # inside quotes are nothing but strings.
+
+# name = 'hyndavi'
+# print(name)
+# print(type(name))  # str
+
+        # Quotes can be of beloow types:
+            # single quotes ==> ''
+            # double quotes ==> ""
+            # tripple single quotes ==> ''' '''
+            # tripple double quotes ==> """ """
+
+# password = 'kiran@123'
+# print(password)
+# print(type(password))  # str
+
+# fruit = "Apple"
+# print(fruit)
+# print(type(fruit))  # str
+
+# thought_of_the_day = '''Apple a day, keeps doctor away..'''
+# print(thought_of_the_day)
+# print(type(thought_of_the_day))
+
+
+# thought_of_the_day_after_tomorrow = """Johny, Johny .. Yes Papa..!"""
+# print(thought_of_the_day_after_tomorrow)
+# print(type(thought_of_the_day_after_tomorrow))
+
+
+# --------------------------
+
+# xyz = 'Bhargav is the "tallest" among all'
+# print(xyz)
+# print(type(xyz))
+
+# mno = "Hyndavi is the 'smartest' among all"
+# print(mno)
+# print(type(mno))
+
+
+# para = '''Hello This is shiv from 'hyderababd'..!
+# I am a "software engineer".
+# You can also be a "software engineer".
+# """learn python""" and get job..'''
+
+# print(para)
+
+# ------------What type of data type is String:-
+
+    # String is sequesnce of charecters.
+    # String is ordered datatypes 
+    # String also supports indexing
+    # String is Immutable Datatype  # Cant be update or modified
+
+# x = 'Superman'
+
+# INDEXING:
+
+# name = "Sridhar"
+# print(name)
+
+# print(name[0])
+# print(name[1])
+# print(name[4])
+# print(name[6])
+
+# print(name[-7])
+# print(name[-4])
+
+#--------------------------------
+
+# age = 56   # int
+# weight = '77'  # str
+
+# pqr = '1234567'
+# print(pqr[6])
+# print(pqr[10])  #IndexError: string index out of range
+
+
+#--------------------------
+
+# SLICING:-
+
+friend = "Mallesh"
+# print(friend)
+# print(friend[3])
+
+# les
+# print(friend[3:6])  # [start: end]
+# print(friend[-4:-1])
+
+# Mleh
+# friend = "Mallesh"
+# print(friend[0:7:2]) # [start: end: step]  #Mleh
+# print(friend[0::2])  #Mleh
+
+# # as
+# print(friend[1:6:4])
+
+
+str1 = "ABCDEFGH"
+
+# BEH
+# print(str1[1:8:3])
+# print(str1[0:6:5])
+# print(str1[6::-3]) #GDA
+
+str1 = "ABCDEFGH"
+# # HGFE
+# print(str1[-1:-5:-1])
+
+# ABCD
+# print(str1[0:4:1])
+# print(str1[0:4:])
+
