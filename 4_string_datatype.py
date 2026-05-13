e8973f8d09b3c571cd8982306bfb169e1e964b25
shivmatkawala <shivkumarmatkawala@gmail.com>
2026-05-13 22:33:42 +0530

    strated collections

--- 4_string_datatype.py
+++ 4_string_datatype.py
@@ -117,3 +117,133 @@ str1 = "ABCDEFGH"
 # print(str1[0:4:1])
 # print(str1[0:4:])
 
+# ------------------- In-built Methods of string
+
+# Functions ==> Methods
+
+# Methods are useful to perform operations on object (string):
+
+# Behind every method there is a program.
+
+# Two types of Methods:
+        # In Built Method
+        # User Defined Methods
+
+# str1 = "apple@123"
+
+# Case Transformation Operations:
+
+# .upper()  => Converts all lower case alphabets into uppercase.
+
+# print(str1.upper())  #APPLE@123
+
+# str2 = "GRAPE%8090"
+# .lower()
+
+# print(str2.lower())  #grape%8090
+
+# str3 = "AjAy MisrA"
+# print(str3.upper())  #AJAY MISRA
+# print(str3.lower())  #ajay misra
+
+
+# str4 = "davID warNer is the best BATSMAN"
+
+# .title()  it converts strings each words first alphabetic letter to uppercae
+# and rest of the letters to lowercase.
+
+# print(str4.title()) #David Warner Is The Best Batsman
+
+
+# .capitalize()  it converts only first letter of teh string to capital 
+# and rest will be lowercase.
+
+# str5 = "Donald TRUMP is the Best PresideNT Ever"
+# print(str5.capitalize())  #Donald trump is the best president ever
+
+
+# str6 = "PYTHON programming"
+# .swapcase()  it converts uppercase letters into lowercase
+# and lowercase into uppercase
+
+# print(str6.swapcase())  #python PROGRAMMING
+
+
+#--------------------------------------
+
+
+# str7 = "   aaa8..AA"
+
+# a.A
+# print(str7[4:11:3])
+# print(str7[-1:-8:-3].swapcase())
+
+# ----------------- Search methods
+
+# str8 = "kdjbdwbdieuwfwz 9eu3209j32j 39hd    2 hd3   28gy dhqehduygd"
+
+# .index()  It seraches from left and returns the 
+# first specified substring  index
+# print(str8.index('d')) 
+
+# # .rindex() this searches from right side
+# print(str8.rindex('d'))  # 58
+
+# print(str8.index('Z'))  #ValueError: substring not found
+
+
+#-------------
+# .find()  It seraches from left and returns the 
+# first specified substring  index but 
+# if substring is not available, it returns -1
+
+# print(str8.find('d'))
+
+# # .rfind()
+# print(str8.rfind('d'))
+
+# print(str8.find('Z'))  # -1
+# print(str8.rfind('Z'))  # -1
+
+#----------------------------
+
+# .isdigit()
+# .isalpha()
+# .isalnum()
+
+# str9 = "1234"
+# print(str9.isdigit())
+
+# str10 = "abcd"
+# print(str10.isdigit())
+
+# str11 = "klm123"
+# print(str11.isdigit())
+
+# str12 = "abcke"
+# print(str12.isalpha())
+
+# pstr13 = "gddTY."
+# print(pstr13.isalpha())
+
+# str14 = "123asd"
+# print(str14.isalnum())
+
+# str15 = "12345"
+# print(str15.isalnum())
+
+#---------------------------
+
+# some other operations
+
+# Repetation
+s1 = "apple"
+print(s1 * 2)
+
+
+# concatination
+s2 = "grape"
+print(s1 + s2)
+
+
+# packing & unpacking not supported by string
---
+++ 5_collection_datatypes.py
@@ -0,0 +1,50 @@
+# Collections:
+
+    # List:-
+        # List is a collection datatype
+        # List is heterogeneous datatype [it can contain variety of data]
+        # List is ordered collection
+        # List supports indexing
+        # List is mutable datatype  [can be modified after its initialization]
+        # List can contain duplicates
+
+# How to create a list ?
+
+    # using []
+    # using list()
+
+list1 = [1, 2, 3, 4, 5.5, 1, 'Hello']
+print(list1)
+print(type(list1))  # list
+
+print(list1[4])
+print(list1[-3])
+
+print(list1[-1])
+print(list1[6])
+
+# Nested Indexing
+print(list1[6][1])
+print(list1[6][-4])
+
+# lle
+print(list1[-1][-2:-5:-1])
+
+# list in-built methods:
+
+    # Tuple:-
+        # Tuple is also collection datatype
+        # Tuple is also heterogeneous in nature
+        # Tuple is also ordered
+        # Tuple supports indexing
+        # Tuple is immutable [cant be modified ever after initialization]
+        # Tuple also can contain duplicates
+
+
+    # Set:-
+        # Set is also a collection dataype
+        # Set is partially heterogeneous [only immutable data]
+        # Set itself is mutable
+        # Set is unorderd 
+        # Set doest support indexing
+        # Set doesnt allow duplicates
