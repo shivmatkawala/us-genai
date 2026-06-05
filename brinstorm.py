44f5ed423a758c9b5b8ec3d51f4443210250703e
shivmatkawala <shivkumarmatkawala@gmail.com>
2026-06-05 22:30:25 +0530

    started matplotlib

--- brinstorm.py
+++ brinstorm.py
@@ -1,3 +1,13 @@
 # Write a function to get the 
 # prime numbers in between 1 and 100.
 
+
+# AMGEN:-
+list1 = [97, 86, 97, 86, 95, 32, 16, 97, 97, 86, 45, 101]
+# Find the number which appered maximum times.
+
+# 4 ways to solve.
+    # While loop
+    # for loop
+    # lambda 
+    # statistics module ==> mode
